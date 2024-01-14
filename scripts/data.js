f3b0e0a8e239122ca4fd39d6a6bc0a706355c442
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-01-14 14:25:12 +0700

    inisialisasi git

---
+++ scripts/data.js
@@ -0,0 +1,27 @@
+export const COFFEE_DATA = [
+  {
+    name: "Kopi Moccha",
+    image: "https://source.unsplash.com/random/150x150?moccha-coffee",
+    price: 12000,
+  },
+  {
+    name: "Kopi Cappuccino",
+    image: "https://source.unsplash.com/random/150x150?cappuccino-coffee",
+    price: 12000,
+  },
+  {
+    name: "Kopi Nescafe",
+    image: "https://source.unsplash.com/random/150x150?nescafe-coffee",
+    price: 12000,
+  },
+  {
+    name: "Kopi Americano",
+    image: "https://source.unsplash.com/random/150x150?americano-coffee",
+    price: 12000,
+  },
+  {
+    name: "Kopi Luwak",
+    image: "https://source.unsplash.com/random/150x150?luwak-coffee",
+    price: 12000,
+  },
+];
